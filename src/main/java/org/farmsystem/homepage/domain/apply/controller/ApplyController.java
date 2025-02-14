2d28591340018dddfbf211b6183e1381c04342c9
Jinsoo Yoon <floreo1242@gmail.com>
2025-02-15 05:31:22 +0900

    [feat] #14 Apply 컨트롤러 생성

---
+++ src/main/java/org/farmsystem/homepage/domain/apply/controller/ApplyController.java
@@ -0,0 +1,44 @@
+package org.farmsystem.homepage.domain.apply.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.farmsystem.homepage.domain.apply.dto.CreateApplyRequestDto;
+import org.farmsystem.homepage.domain.apply.dto.ApplyRequestDto;
+import org.farmsystem.homepage.domain.apply.dto.LoadApplyRequestDto;
+import org.farmsystem.homepage.domain.apply.service.ApplyService;
+import org.farmsystem.homepage.global.common.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/apply")
+public class ApplyController {
+
+    private final ApplyService applyService;
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> getQuestions() {
+        return SuccessResponse.ok(applyService.getQuestions());
+    }
+
+    @PostMapping("/info")
+    public ResponseEntity<SuccessResponse<?>> createApply(@RequestBody @Valid CreateApplyRequestDto request) {
+        return SuccessResponse.created(applyService.createApply(request));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<SuccessResponse<?>> saveApply(@RequestBody @Valid ApplyRequestDto request) {
+        return SuccessResponse.ok(applyService.saveApply(request, false));
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<SuccessResponse<?>> submitApply(@RequestBody @Valid ApplyRequestDto request) {
+        return SuccessResponse.ok(applyService.saveApply(request, true));
+    }
+
+    @PostMapping("/load")
+    public ResponseEntity<SuccessResponse<?>> loadApply(@RequestBody @Valid LoadApplyRequestDto request) {
+        return SuccessResponse.ok(applyService.loadApply(request));
+    }
+}
