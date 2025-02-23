ff1852e9fb9fa31013b0f78b4f110d81fd7e0ce2
hysong4u <hayeon_song@naver.com>
2025-02-24 02:42:17 +0900

    [refactor] #25 204 상태에 대한 SuccessResponse 생성 & 반영

--- src/main/java/org/farmsystem/homepage/global/common/SuccessCode.java
+++ src/main/java/org/farmsystem/homepage/global/common/SuccessCode.java
@@ -16,7 +16,12 @@ public enum SuccessCode {
     /**
      * 201 Created
      */
-    CREATED(HttpStatus.CREATED, "요청이 성공했습니다.");
+    CREATED(HttpStatus.CREATED, "요청이 성공했습니다."),
+
+    /**
+     * 204 No Content
+     */
+    NO_CONTENT(HttpStatus.NO_CONTENT, "요청이 성공했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
