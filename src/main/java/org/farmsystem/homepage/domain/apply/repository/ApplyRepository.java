45239ba33af068834a36e8f927a7ab96d2e8e8f7
Jinsoo Yoon <floreo1242@gmail.com>
2025-03-02 17:29:43 +0900

    [refactor] #47 N+1문제 관련 개선
    - Question 조회 시, Choice는 EntityGraph로 fetch join 적용
    - Apply 조회 시, Answer는 fetch join, 각 Answer에 대한 AnswerChoice BatchSize(5)로 조회

--- src/main/java/org/farmsystem/homepage/domain/apply/repository/ApplyRepository.java
+++ src/main/java/org/farmsystem/homepage/domain/apply/repository/ApplyRepository.java
@@ -1,11 +1,13 @@
 package org.farmsystem.homepage.domain.apply.repository;
 
 import org.farmsystem.homepage.domain.apply.entity.Apply;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
 
+    @EntityGraph(attributePaths = {"answers"})
     List<Apply> findAllByStudentNumber(String studentNumber);
 }
--- src/main/java/org/farmsystem/homepage/domain/apply/repository/QuestionRepository.java
+++ src/main/java/org/farmsystem/homepage/domain/apply/repository/QuestionRepository.java
@@ -1,11 +1,13 @@
 package org.farmsystem.homepage.domain.apply.repository;
 
 import org.farmsystem.homepage.domain.apply.entity.Question;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    @EntityGraph(attributePaths = {"choices"})
     List<Question> findAllByOrderByTrackAscPriorityAsc();
 }
