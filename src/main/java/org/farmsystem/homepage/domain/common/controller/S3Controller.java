9e32df0a3ae541241c246bb365e5e737adbddf6c
hysong4u <hayeon_song@naver.com>
2025-03-10 00:17:49 +0900

    [feat] #60 presigned URL 업로드 API 구현 (프로필용, 일반용)

---
+++ src/main/java/org/farmsystem/homepage/domain/common/controller/S3Controller.java
@@ -0,0 +1,32 @@
+package org.farmsystem.homepage.domain.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.farmsystem.homepage.domain.common.dto.request.PresignedUrlRequestDTO;
+import org.farmsystem.homepage.domain.common.dto.response.PresignedUrlResponseDTO;
+import org.farmsystem.homepage.global.common.SuccessResponse;
+import org.farmsystem.homepage.domain.common.service.S3Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/s3")
+@RestController
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    // S3 presigned URL 생성 API
+    @PostMapping("presigned-url")
+    public ResponseEntity<SuccessResponse<?>> generatePresignedUrl(@RequestBody PresignedUrlRequestDTO presignedUrlRequest) {
+        PresignedUrlResponseDTO presignedUrl = s3Service.generatePresignedUrl(presignedUrlRequest);
+        return SuccessResponse.ok(presignedUrl);
+    }
+
+    // 프로필 사진 업로드용 presigned URL 생성 API
+    @PostMapping("presigned-url/profile")
+    public ResponseEntity<SuccessResponse<?>> generateProfilePresignedUrl(@AuthenticationPrincipal Long userId) {
+        PresignedUrlResponseDTO presignedUrl = s3Service.generateProfilePresignedUrl(userId);
+        return SuccessResponse.ok(presignedUrl);
+    }
+}
