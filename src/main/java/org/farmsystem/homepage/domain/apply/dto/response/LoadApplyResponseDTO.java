94a0f0f000c684eb28a1c88ecbb24a8917210e06
Jinsoo Yoon <floreo1242@gmail.com>
2025-03-12 21:21:30 +0900

    [fix] #101 지원서 확인 API에 학번 추가
    - from 메소드 활용 리팩토링

--- src/main/java/org/farmsystem/homepage/domain/apply/dto/response/LoadApplyResponseDTO.java
+++ src/main/java/org/farmsystem/homepage/domain/apply/dto/response/LoadApplyResponseDTO.java
@@ -2,11 +2,13 @@ package org.farmsystem.homepage.domain.apply.dto.response;
 
 import lombok.Builder;
 import org.farmsystem.homepage.domain.apply.dto.AnswerDTO;
+import org.farmsystem.homepage.domain.apply.entity.Apply;
 import org.farmsystem.homepage.domain.apply.entity.ApplyStatusEnum;
 import org.farmsystem.homepage.domain.common.entity.Track;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 public record LoadApplyResponseDTO(
@@ -15,9 +17,32 @@ public record LoadApplyResponseDTO(
         LocalDateTime updatedAt,
         String name,
         String major,
+        String studentNumber,
         String phoneNumber,
         String email,
         Track track,
         List<AnswerDTO> answers
 ) {
+    public static LoadApplyResponseDTO from(Apply apply) {
+        return LoadApplyResponseDTO.builder()
+                .applyId(apply.getApplyId())
+                .status(apply.getStatus())
+                .updatedAt(apply.getUpdatedAt())
+                .name(apply.getName())
+                .major(apply.getMajor())
+                .studentNumber(apply.getStudentNumber())
+                .phoneNumber(apply.getPhoneNumber())
+                .email(apply.getEmail())
+                .track(apply.getTrack())
+                .answers(apply.getAnswers().stream()
+                        .map(answer -> AnswerDTO.builder()
+                                .questionId(answer.getQuestion().getQuestionId())
+                                .content(answer.getContent())
+                                .choiceId(answer.getAnswerChoices().stream()
+                                        .map(answerChoice -> answerChoice.getChoice().getChoiceId())
+                                        .collect(Collectors.toList()))
+                                .build())
+                        .collect(Collectors.toList()))
+                .build();
+    }
 }
