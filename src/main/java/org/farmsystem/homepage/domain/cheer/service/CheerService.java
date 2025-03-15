f17fc07b9b631412ee0299b65e133a8546d397f1
Jinsoo Yoon <floreo1242@gmail.com>
2025-03-15 21:48:59 +0900

    [feat] #54 응원 등록 API

--- src/main/java/org/farmsystem/homepage/domain/cheer/service/CheerService.java
+++ src/main/java/org/farmsystem/homepage/domain/cheer/service/CheerService.java
@@ -1,10 +1,15 @@
 package org.farmsystem.homepage.domain.cheer.service;
 
 import lombok.RequiredArgsConstructor;
+import org.farmsystem.homepage.domain.cheer.dto.request.CheerRequestDTO;
 import org.farmsystem.homepage.domain.cheer.dto.response.CheerResponseDTO;
 import org.farmsystem.homepage.domain.cheer.dto.response.PagingCheerListResponseDTO;
 import org.farmsystem.homepage.domain.cheer.entity.Cheer;
 import org.farmsystem.homepage.domain.cheer.repository.CheerRepository;
+import org.farmsystem.homepage.domain.user.entity.User;
+import org.farmsystem.homepage.domain.user.repository.UserRepository;
+import org.farmsystem.homepage.global.error.ErrorCode;
+import org.farmsystem.homepage.global.error.exception.BusinessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,6 +24,7 @@ import java.util.stream.Collectors;
 public class CheerService {
 
     private final CheerRepository cheerRepository;
+    private final UserRepository userRepository;
 
     // 응원 리스트 조회 (페이징)
     public PagingCheerListResponseDTO getAllCheer(Pageable pageable) {
@@ -27,5 +33,22 @@ public class CheerService {
                 .map(CheerResponseDTO::from)
                 .collect(Collectors.toList());
         return PagingCheerListResponseDTO.of(cheerPage, pageable, cheerList);
+    }
+
+    // 응원 등록
+    @Transactional
+    public CheerResponseDTO createCheer(CheerRequestDTO request) {
+        User cheerer = userRepository.findById(request.cheererId())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        User cheered = userRepository.findById(request.cheeredId())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        Cheer cheer = Cheer.builder()
+                .cheerer(cheerer)
+                .cheered(cheered)
+                .content(request.content())
+                .tag(request.tag())
+                .build();
+        Cheer savedCheer = cheerRepository.save(cheer);
+        return CheerResponseDTO.from(savedCheer);
     }
 }
