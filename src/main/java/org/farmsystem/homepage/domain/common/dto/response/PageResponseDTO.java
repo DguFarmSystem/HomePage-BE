3a7c22eb690f8a4ab6da371b9a1605b91fed0284
hysong4u <hayeon_song@naver.com>
2025-03-22 02:00:19 +0900

    [refactor] #96 PageResponseDTO page 객체로 생성(pageable 안쓰고 파라미터 하나로 통일) & 이전/이후 페이지 존재 여부 값 추가

--- src/main/java/org/farmsystem/homepage/domain/common/dto/response/PageResponseDTO.java
+++ src/main/java/org/farmsystem/homepage/domain/common/dto/response/PageResponseDTO.java
@@ -1,22 +1,25 @@
 package org.farmsystem.homepage.domain.common.dto.response;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public record PageResponseDTO(
-        int totalElements,
-        int pageSize,
-        int totalPages,
-        int currentPage,
-        String sortBy
+        int totalElements, // 총 데이터 개수
+        int pageSize, // 페이지당 데이터 개수
+        int totalPages, // 총 페이지 개수
+        int currentPage, // 현재 페이지
+        String sortBy, // 정렬 기준
+        boolean hasNextPage, // 다음 페이지 존재 여부
+        boolean hasPreviousPage // 이전 페이지 존재 여부
 ) {
-    public static PageResponseDTO of(Page<?> page, Pageable pageable) {
+    public static PageResponseDTO of(Page<?> page) {
         return new PageResponseDTO(
                 (int) page.getTotalElements(),
-                pageable.getPageSize(),
+                page.getSize(),
                 page.getTotalPages(),
-                pageable.getPageNumber() + 1,
-                pageable.getSort().toString()
+                page.getNumber()+1,
+                page.getSort().toString(),
+                page.hasNext(),
+                page.hasPrevious()
         );
     }
 }
