a6c757c5e00e7067c66b4bae7ce09ad1693e9cc2
saokiritoni <soeun.love.lee@gmail.com>
2025-03-22 18:15:06 +0900

    [feat] 필요없는 DTO 삭제

--- src/main/java/org/farmsystem/homepage/domain/project/dto/response/MyApplicationResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package org.farmsystem.homepage.domain.project.dto.response;
-
-public record MyApplicationResponseDTO(
-        Long projectId,
-        String title,
-        String approvalStatus
-) {}
