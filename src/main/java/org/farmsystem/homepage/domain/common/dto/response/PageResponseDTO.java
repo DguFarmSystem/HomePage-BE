2bfc1e73da5d98b2150f5590756dd8cc63f5ee60
hysong4u <hayeon_song@naver.com>
2025-03-22 15:43:56 +0900

    [docs] #96 pageResponseDTO 스키마 설명 추가

--- src/main/java/org/farmsystem/homepage/domain/common/dto/response/PageResponseDTO.java
+++ src/main/java/org/farmsystem/homepage/domain/common/dto/response/PageResponseDTO.java
@@ -1,20 +1,32 @@
 package org.farmsystem.homepage.domain.common.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.data.domain.Page;
 
 public record PageResponseDTO(
-        int totalElements, // 총 데이터 개수
-        int pageSize, // 페이지당 데이터 개수
-        int totalPages, // 총 페이지 개수
-        int currentPage, // 현재 페이지
-        String sortBy, // 정렬 기준
-        boolean hasNextPage, // 다음 페이지 존재 여부
-        boolean hasPreviousPage // 이전 페이지 존재 여부
+
+        @Schema(description = "페이지당 데이터 개수")
+        int pageSize,
+        @Schema(description = "총 데이터 개수")
+        int totalElements,
+        @Schema(description = "현재 페이지 데이터 개수")
+        int currentPageElements,
+        @Schema(description = "총 페이지 개수")
+        int totalPages,
+        @Schema(description = "현재 페이지")
+        int currentPage,
+        @Schema(description = "정렬 기준")
+        String sortBy,
+        @Schema(description = "다음 페이지 존재 여부")
+        boolean hasNextPage,
+        @Schema(description = "이전 페이지 존재 여부")
+        boolean hasPreviousPage
 ) {
     public static PageResponseDTO of(Page<?> page) {
         return new PageResponseDTO(
-                (int) page.getTotalElements(),
                 page.getSize(),
+                (int) page.getTotalElements(),
+                page.getNumberOfElements(),
                 page.getTotalPages(),
                 page.getNumber()+1,
                 page.getSort().toString(),
