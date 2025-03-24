54e89a7a7bba8e4e8ba08979cb4d23bc8678a4f2
hysong4u <hayeon_song@naver.com>
2025-03-24 15:09:58 +0900

    [feat] #63 사용자 랭킹 조회 Controller 구현

--- src/main/java/org/farmsystem/homepage/domain/user/controller/UserController.java
+++ src/main/java/org/farmsystem/homepage/domain/user/controller/UserController.java
@@ -5,9 +5,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.farmsystem.homepage.domain.user.dto.request.UserUpdateRequestDTO;
 import org.farmsystem.homepage.domain.user.dto.request.UserVerifyRequestDTO;
-import org.farmsystem.homepage.domain.user.dto.response.OtherUserInfoResponseDTO;
-import org.farmsystem.homepage.domain.user.dto.response.UserInfoResponseDTO;
-import org.farmsystem.homepage.domain.user.dto.response.UserVerifyResponseDTO;
+import org.farmsystem.homepage.domain.user.dto.response.*;
+import org.farmsystem.homepage.domain.user.service.UserRankingService;
 import org.farmsystem.homepage.domain.user.service.UserService;
 import org.farmsystem.homepage.global.common.SuccessResponse;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController implements UserApi {
     private final UserService userService;
+    private final UserRankingService userRankingService;
 
     // 사용자 회원 인증 API
     @PostMapping("/verify")
@@ -59,5 +59,12 @@ public class UserController implements UserApi {
     @GetMapping("/suggest")
     public ResponseEntity<SuccessResponse<?>> searchUserSuggest(@RequestParam String query) {
         return SuccessResponse.ok(userService.searchUserSuggest(query));
+    }
+
+    // 사용자 랭킹
+    @GetMapping("/ranking")
+    public ResponseEntity<SuccessResponse<?>> getUserRanking(@AuthenticationPrincipal Long userId) {
+        UserRankListResponseDTO userRankList = userRankingService.getDailyRanking(userId);
+        return SuccessResponse.ok(userRankList);
     }
 }
