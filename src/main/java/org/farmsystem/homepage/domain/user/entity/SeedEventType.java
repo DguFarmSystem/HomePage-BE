00b6e67b4d83df70123fc7047ef7b8b89c5a46a4
hysong4u <hayeon_song@naver.com>
2025-03-25 00:38:06 +0900

    [feat] #63 데일리 씨앗 적립 엔티티 구현

---
+++ src/main/java/org/farmsystem/homepage/domain/user/entity/SeedEventType.java
@@ -0,0 +1,14 @@
+package org.farmsystem.homepage.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SeedEventType {
+    ATTENDANCE(1),   // 출석 1개
+    CHEER(2),        // 응원 2개
+    FARMING_LOG(5);  // 파밍로그 5개
+
+    private final int seedAmount;
+}
