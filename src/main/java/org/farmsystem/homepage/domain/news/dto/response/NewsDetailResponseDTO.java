61ccbaa52e3af45336f81e087e84c7f3e1c593d7
saokiritoni <soeun.love.lee@gmail.com>
2025-05-18 15:14:10 +0900

    [refactor] #89 fromEntity로 수정

--- src/main/java/org/farmsystem/homepage/domain/news/dto/response/NewsDetailResponseDTO.java
+++ src/main/java/org/farmsystem/homepage/domain/news/dto/response/NewsDetailResponseDTO.java
@@ -1,5 +1,17 @@
 package org.farmsystem.homepage.domain.news.dto.response;
 
+import org.farmsystem.homepage.domain.news.entity.News;
+
 import java.util.List;
 
-public record NewsDetailResponseDTO(Long newsId, String title, String thumbnailUrl, String content, List<String> imageUrls) { }
+public record NewsDetailResponseDTO(Long newsId, String title, String thumbnailUrl, String content, List<String> imageUrls) {
+    public static NewsDetailResponseDTO fromEntity(News news) {
+        return new NewsDetailResponseDTO(
+                news.getNewsId(),
+                news.getTitle(),
+                news.getThumbnailUrl(),
+                news.getContent(),
+                news.getImageUrls()
+        );
+    }
+}
--- src/main/java/org/farmsystem/homepage/domain/news/dto/response/NewsListResponseDTO.java
+++ src/main/java/org/farmsystem/homepage/domain/news/dto/response/NewsListResponseDTO.java
@@ -1,3 +1,9 @@
 package org.farmsystem.homepage.domain.news.dto.response;
 
-public record NewsListResponseDTO(Long newsId, String title, String thumbnailUrl) { }
+import org.farmsystem.homepage.domain.news.entity.News;
+
+public record NewsListResponseDTO(Long newsId, String title, String thumbnailUrl) {
+    public static NewsListResponseDTO fromEntity(News news) {
+        return new NewsListResponseDTO(news.getNewsId(), news.getTitle(), news.getThumbnailUrl());
+    }
+}
