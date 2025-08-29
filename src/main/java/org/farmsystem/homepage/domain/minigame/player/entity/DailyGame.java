36041c01af5ebaff9450ec49785e743f8736e57b
saokiritoni <soeun.love.lee@gmail.com>
2025-08-29 15:35:58 +0900

    [refactor] #203 주석 스타일 수정

--- src/main/java/org/farmsystem/homepage/domain/minigame/player/entity/DailyGame.java
+++ src/main/java/org/farmsystem/homepage/domain/minigame/player/entity/DailyGame.java
@@ -36,11 +36,17 @@ public class DailyGame {
     @JoinColumn(name = "player_id", nullable = false)
     private Player player;
 
+
+    // ==== 비즈니스 메서드 ====
+
     public static DailyGame createDailyGame(Player player) {
         return new DailyGame(player, 3, 3, 3, LocalDate.now());
     }
 
-    // 날짜가 바뀌었으면 횟수를 초기화한다 (다시 3회로 초기화)
+    /**
+     * 날짜가 바뀌었으면 횟수를 다시 3회로 초기화
+     */
+
     public void resetIfNeeded() {
         if (!LocalDate.now().equals(this.lastResetDate)) {
             resetCounts();
@@ -53,7 +59,9 @@ public class DailyGame {
         this.lastResetDate = LocalDate.now();
     }
 
-    // 게임 횟수를 1회 차감(0보다 작아질 수 없음)
+    /**
+     *  게임 횟수를 1회 차감 (0보다 작아질 수 없음)
+     */
     public void useGame(String gameType) {
         switch (gameType.toLowerCase()) {
             case "rock" -> {
@@ -71,7 +79,10 @@ public class DailyGame {
             default -> throw new BusinessException(ErrorCode.INVALID_GAME_TYPE);
         }
     }
-    // 현재 게임의 남은 횟수 반환
+
+    /**
+     * 현재 게임의 남은 횟수 반환
+     */
     public int getRemainingCount(String gameType) {
         return switch (gameType.toLowerCase()) {
             case "rock" -> this.rockScissors;
